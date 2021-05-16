e75f5182b39d9a881e43be5040ecdc84dd57d416
bnv20 <amicobnv20@gmail.com>
2021-05-16 16:01:24 +0900

    change home img

--- ch99/mysite/settings.py
+++ ch99/mysite/settings.py
@@ -121,7 +121,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIRS = [BASE_DIR / 'static']
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
